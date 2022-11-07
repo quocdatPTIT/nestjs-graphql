41922b5c2539322fd740ea653c5cad62670c1684
Tran Quoc Dat <dattranquoc05@gmail.com>
2022-11-07 23:33:38 +0700

    create user decorator

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiBasicAuth, ApiTags } from '@nestjs/swagger';
+import { User } from '../decorators/user.decorator';
 
 import { LoginDto } from '../dtos/login.dto';
+import { JwtAuthGuard } from '../guards/jwt.guard';
 import { AuthService } from '../services/auth.service';
+import { JwtPayloadDto } from '../dtos/jwt-payload.dto';
 
 @ApiTags('auth')
 @Controller('/auth')
@@ -12,5 +15,11 @@ export class AuthController {
   @Post('/login')
   async login(@Body() loginDto: LoginDto) {
     return await this.authService.login(loginDto);
+  }
+
+  @Get('/profile')
+  @UseGuards(JwtAuthGuard)
+  profile(@User() user: JwtPayloadDto) {
+    return user;
   }
 }
